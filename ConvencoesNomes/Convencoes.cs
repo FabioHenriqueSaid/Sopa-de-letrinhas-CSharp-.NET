55d7459dd86d8d2e2ab7092513b2bca44c5d2e96
FabioHenriqueSaid <fabiosaid635@gmail.com>
2019-07-06 16:31:27 -0300

    Convenções de nomes

---
+++ ConvencoesNomes/Convencoes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sopa_de_Letrinhas.ConvencoesNomes//Pascal Case
+{
+    class Convencoes//Pascal Case
+    {
+
+        /*
+           Camel Case: lastName (parametros de metodos,variaveis  dentro de metodos)
+           Pascal Case : LastName (namespaces,classe,properties e métodos)
+           Padrão _lastName (atributos "internos" da classe)
+         */
+
+        public string Titular { get; set; }//properties
+
+        private double _saldo;//atributo interno
+
+        public void Valor(double valor) {
+            //parametro - CamelCase
+            //metodo - Pascal Case
+            _saldo += valor;
+        }
+    }
+}
