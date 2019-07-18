fc98a7e9688537652622487e6ae379ec5fee223f
FabioHenriqueSaid <fabiosaid635@gmail.com>
2019-07-18 07:56:42 -0300

    Entrada de dados

--- Entrada de dados/Entradas.cs
+++ Entrada de dados/Entradas.cs
@@ -1,33 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Sopa_de_Letrinhas.Entrada_de_dados
 {
     class Entradas
     {
-        public void Demonstracao() {
-            string palavra;
-            Console.Write("Digite uma palavra:");
-            palavra = Console.ReadLine(); //Lê até encontrar uma quebra de linha
-            Console.WriteLine(palavra);
-            Split();
+        public void Demonstracao() {            
+            int n1 = int.Parse(Console.ReadLine());//Console.ReadLine lê os dados em forma de String, por consequencia disso é necessario fazer uma conversao entre tipos.
+            Console.WriteLine(n1);
+
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine(n2.ToString("F2",CultureInfo.InvariantCulture));
+
+            
+            Split();//Usando Split
         }
-        public void Split() {
-            string s = Console.ReadLine();
+        public void Split() {        
+            string[] dados = Console.ReadLine().Split(' ');
+            string nome = dados[0];
+            char sexo = char.Parse(dados[1]);
+            int idade = int.Parse(dados[2]);
+            double altura = double.Parse(dados[3], CultureInfo.InvariantCulture);
 
-            string[] v = s.Split(' ');
-            string a = v[0];
-            string b = v[1];
-            string c = v[2];
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-
-            string[] vetor = Console.ReadLine().Split(' ');
-            Console.WriteLine(vetor[0],vetor[1],vetor[2],vetor[3]);
-
+            Console.WriteLine(" Nome: {0}\n Sexo: {1}\n Idade: {2}\n Altura: {3}\n "
+                ,nome , sexo , idade , altura.ToString("F2",CultureInfo.InvariantCulture));
         }
     }
 }
